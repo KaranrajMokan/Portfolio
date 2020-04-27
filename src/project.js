10235176920dc55cc2df1672d347dc3d2eec13a7
KaranrajMokan <karanrajmokan@gmail.com>
2020-04-27 22:32:53 +0530

    Add files via upload

---
+++ src/project.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import './project.css';
+
+
+export default function Project(){
+
+    return(
+
+    <div>
+        <br/><br/><br/><br/><br/><br/>
+        <div className="borders1">
+            <div className="mar1">
+                <div className="mar2">
+                Projects
+                </div>
+            </div>
+        </div>
+        <br/><br/><br/>
+    </div>
+    );
+}
