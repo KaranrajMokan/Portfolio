92c9fc6d05505df9d34b0be3dc929a9e5f76a346
KaranrajMokan <karanrajmokan@gmail.com>
2020-04-27 22:31:40 +0530

    Add files via upload

---
+++ src/intro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+export default function Intro(){
+
+    return(
+        <div>
+            <h1 className="gap">
+            Hello there! <br />
+            Welcome
+            </h1>
+        </div>
+
+    )
+}
