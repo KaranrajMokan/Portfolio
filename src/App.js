7f0c684fb7d0e6ecba37b6593da5c97ad7321a19
KaranrajMokan <karanrajmokan@gmail.com>
2020-04-27 22:29:59 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
 import './App.css';
 import me from './me.png';
+import Intro from './intro';
 import About from './about';
+import Project from './project';
+import { Container, Row, Col } from 'reactstrap';
+
+
 
 class App extends React.Component {
 
@@ -11,7 +16,11 @@ class App extends React.Component {
     this.state = { value: 0};
     this.colorchange = this.colorchange.bind(this);
     this.colorchangeback = this.colorchangeback.bind(this);
-
+    this.call0 = this.call0.bind(this);
+    this.call1 = this.call1.bind(this);
+    this.call2 = this.call2.bind(this);
+    this.call3 = this.call3.bind(this);
+    this.call4 = this.call4.bind(this);
   }
 
   colorchange(e){
@@ -22,41 +31,64 @@ class App extends React.Component {
     e.target.style.color = 'black';
   }
 
+  call0(e)
+  {
+    e.target.size = '100px';
+    this.setState({value: 0});
+  }
+
+  call1(){
+    this.setState({value: 1});
+  }
+
+  call2(){
+    this.setState({value: 2});
+  }
+
+  call3(){
+    this.setState({value: 3});
+  }  
+
+  call4(){
+    this.setState({value: 4});
+  }
 
   render(){
 
+    let a;
+    if(this.state.value === 0)
+      a = <Intro />
+    else if(this.state.value === 1)
+      a = <About />
+    else if(this.state.value === 2)
+      a = <Project />
   return(
-    <div>
-      <div className="left">
-        <div className="banner banner-left">
-          <img src={me} className="App-logo" alt="me" />
-        </div>
-        
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <div className="fonting">
-            <br/>
-            <h3 onMouseEnter={this.colorchange} onMouseLeave={this.colorchangeback} onClick={this.props.call1}>ABOUT</h3>
-            <h3 onMouseEnter={this.colorchange} onMouseLeave={this.colorchangeback} onClick={this.callB}>PROJECTS</h3>
-            <h3 onMouseEnter={this.colorchange} onMouseLeave={this.colorchangeback} onClick={this.callC}>PASSION</h3>
-            <h3 onMouseEnter={this.colorchange} onMouseLeave={this.colorchangeback} onClick={this.callD}>CONTACT</h3><br/>  
-        </div>
-      </div>
+    <Container>
+      <Row>
+        <Col>
+          <nav>
+            <div className="left">
+                <img src={me} className="App-logo" alt="me" />
+                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+                <div className="fonting">
+                  <br/>
+                  <h3 onMouseEnter={this.colorchange} onMouseLeave={this.colorchangeback} onClick={this.call0}>Home</h3>
+                  <h3 onMouseEnter={this.colorchange} onMouseLeave={this.colorchangeback} onClick={this.call1}>ABOUT</h3>
+                  <h3 onMouseEnter={this.colorchange} onMouseLeave={this.colorchangeback} onClick={this.call2}>PROJECTS</h3>
+                  <h3 onMouseEnter={this.colorchange} onMouseLeave={this.colorchangeback} onClick={this.call3}>PASSION</h3>
+                  <h3 onMouseEnter={this.colorchange} onMouseLeave={this.colorchangeback} onClick={this.call4}>CONTACT</h3><br/>
+                </div>
+            </div>
+          </nav>
+        </Col>
 
-      
-      <div className="App-header">
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <h1 className="gap">
-          Hello there! <br />
-          Welcome
-        </h1>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/><br/><br/><br/>
-        <br/><br/><br/><br/><br/><br/><br/>
-        <h1><About /></h1>
-      </div>
-
-    </div>
+        <Col>
+          <div className="App-header">
+            {a}
+          </div>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 }
