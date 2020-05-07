4cb7a379b4a1d79f7127d38edce0137ab3c73daa
KaranrajMokan <karanrajmokan@gmail.com>
2020-05-07 22:44:03 +0530

    Add files via upload

---
+++ src/passion.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './passion.css';
+
+
+export default function Contact(){
+
+    return(
+
+    <div>
+        <br/><br/><br/><br/><br/><br/>
+        <div className="bod">
+            <div className="mar1">
+                <div className="mar2">
+                Passion &nbsp;
+                </div>
+                <div className="mar_pass">
+                    I'm very passionate about the backend technologies and I find myself deeply 
+                    interested in efficient means of data storage and retrieval.
+                    I'm also curious about the correlation between various types of frontend
+                    and backend. I have always wanted to work with NoSQL databases
+                    and thus used Google Firebase's Cloud Firestore (document based) for 
+                    an andriod application and Neo4j (graph based) for a restaurant recommendation system.
+                    
+                </div>
+            </div>
+            <div>
+
+            </div>
+        </div>
+        <br/><br/><br/>
+    </div>
+    );
+}
