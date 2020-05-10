5dcb8b97b3e009549761a5acedb85445fddcbff6
Karanraj Mokan <karanrajmokan@gmail.com>
2020-05-10 19:52:51 +0530

    Updates

---
+++ precache-manifest.31b737f19556f8e5053b1b8bfb178809.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85ed47cb673ce5ec8340b6e7c5124782",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "66e567c762ff707ccb98",
+    "url": "/Portfolio/static/css/main.3eb08038.chunk.css"
+  },
+  {
+    "revision": "6b05af797653c58d8a97",
+    "url": "/Portfolio/static/js/2.ac714747.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Portfolio/static/js/2.ac714747.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e567c762ff707ccb98",
+    "url": "/Portfolio/static/js/main.284c6e12.chunk.js"
+  },
+  {
+    "revision": "6dc4c28c1f82adb5a615",
+    "url": "/Portfolio/static/js/runtime-main.f0dfcb8d.js"
+  },
+  {
+    "revision": "56adab7ae2021ccb6a863528bde099f5",
+    "url": "/Portfolio/static/media/Antique.56adab7a.otf"
+  },
+  {
+    "revision": "d7f33cd76c9a2046abc27a237b017872",
+    "url": "/Portfolio/static/media/Belladona.d7f33cd7.ttf"
+  },
+  {
+    "revision": "f6d0b2bb22521232ba7e7b0f2ceeea81",
+    "url": "/Portfolio/static/media/Comodo Vintage.f6d0b2bb.ttf"
+  },
+  {
+    "revision": "4f597142dd45e50333b6261469d38a00",
+    "url": "/Portfolio/static/media/Coventry.4f597142.ttf"
+  },
+  {
+    "revision": "4e046cecabc4b921d88d2a6070ecd517",
+    "url": "/Portfolio/static/media/FallIsComing.4e046cec.ttf"
+  },
+  {
+    "revision": "a43cbd9728dcc042304e853c8552f3fd",
+    "url": "/Portfolio/static/media/Foglihten.a43cbd97.otf"
+  },
+  {
+    "revision": "e6e964ae821ea56b276ddcd8f34f181f",
+    "url": "/Portfolio/static/media/Geratis.e6e964ae.otf"
+  },
+  {
+    "revision": "316ee285f7fbbf7d9b6781e4daa2a02c",
+    "url": "/Portfolio/static/media/Halimun.316ee285.ttf"
+  },
+  {
+    "revision": "f04bd58dbb01fa4c0b99891a350302b1",
+    "url": "/Portfolio/static/media/LaborUnion.f04bd58d.otf"
+  },
+  {
+    "revision": "dbe9a3b50de6aeb2968f1d751c37ba36",
+    "url": "/Portfolio/static/media/Lustro.dbe9a3b5.ttf"
+  },
+  {
+    "revision": "1fd1346e06cf25dfbbeea8d859926995",
+    "url": "/Portfolio/static/media/RetroSans.1fd1346e.ttf"
+  },
+  {
+    "revision": "f76eaf0e2a876075f5e7a76c33f00518",
+    "url": "/Portfolio/static/media/Silverfake.f76eaf0e.ttf"
+  },
+  {
+    "revision": "30dc96999f7b43461fe83385d4de28ae",
+    "url": "/Portfolio/static/media/Underwood.30dc9699.ttf"
+  },
+  {
+    "revision": "3692b473adb520d88e5eb5fdd29e3d57",
+    "url": "/Portfolio/static/media/Weston.3692b473.ttf"
+  },
+  {
+    "revision": "6bb2b84eaf601eb8529fc5ab3ad8b7e6",
+    "url": "/Portfolio/static/media/dum9.6bb2b84e.jpg"
+  },
+  {
+    "revision": "d22d766717babad7cdc74ce6ba3a4902",
+    "url": "/Portfolio/static/media/dummy8.d22d7667.jpg"
+  },
+  {
+    "revision": "5c4f9465c1e6baaccbf356833bd19867",
+    "url": "/Portfolio/static/media/karan.5c4f9465.jpg"
+  }
+]);
