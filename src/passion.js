46951079db523dc0c72aba2b46efcde86afe94f0
KaranrajMokan <karanrajmokan@gmail.com>
2020-05-10 18:28:20 +0530

    Update passion.js

--- src/passion.js
+++ src/passion.js
@@ -2,16 +2,24 @@ import React from 'react';
 import './passion.css';
 
 
-export default function Contact(){
+export default function Passion(){
+
+    function hello(e){
+        e.target.style.backgroundSize = '100% 88%';
+    }
+
+    function bye(e){
+        e.target.style.backgroundSize = '';
+    }
 
     return(
 
     <div>
         <br/><br/><br/><br/><br/><br/>
-        <div className="bod">
+        <div className="bod module-border-wrap">
             <div className="mar1">
                 <div className="mar2">
-                Passion &nbsp;
+                <span className="underline--magical" onMouseEnter={hello} onMouseLeave={bye}>Passion</span>
                 </div>
                 <div className="mar_pass">
                     I'm very passionate about the backend technologies and I find myself deeply 
@@ -20,7 +28,16 @@ export default function Contact(){
                     and backend. I have always wanted to work with NoSQL databases
                     and thus used Google Firebase's Cloud Firestore (document based) for 
                     an andriod application and Neo4j (graph based) for a restaurant recommendation system.
-                    
+
+                </div>
+                <br/>
+                <div className="mar2">
+                    <span className="underline--magical" onMouseEnter={hello} onMouseLeave={bye}>Experience</span>
+                </div>
+                <div className="mar_pass">
+                    I worked as an intern at D'Edge Promotions,Coimbatore during the month of May 2019.
+                    I was assigned the app development project "Plan-A-Wedding" where I used Kotlin for the frontend and Google Firebase's 
+                    Cloud Firestore for the backend.
                 </div>
             </div>
             <div>
